e4bb02c0b7663d84c0be9f4008234e30cb73bfcd
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 17:28:34 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "user_name" {
+  type = string
+  default = ""
+}
+
+variable "user_password" {
+  type = string
+  default = ""
+}
+
+variable "hashirg" {
+  type = string
+  default = ""
+}
+
+variable "hashiregion" {
+  type = string
+  default = ""
+}
