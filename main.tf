950366b429376f8161c40c2ce452939fd38360b3
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-30 11:00:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,11 +80,12 @@ resource "azurerm_virtual_machine" "radditvm" {
   }
 }
 
-resource "time_sleep" "2_min_wait" {
+resource "time_sleep" "wait_2_min" {
   depends_on = [azurerm_virtual_machine.radditvm]
   create_duration = "120s"
 }
 
 output "public_ip" {
+ depends_on = [time_sleep.wait_2_min]
  value = azurerm_public_ip.hashipubip.ip_address
 }
